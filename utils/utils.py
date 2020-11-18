4dea0ba63edd67df66ff5a57b5eebd32dd00f076
Greer <gviau2@gmail.com>
2020-11-17 21:54:48 -0500

    updating gstreamer pipeline

--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,14 @@
 import cv2
 
-def gstreamer_pipeline(
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=60,
-    flip_method=0):
+def gstreamer_pipeline(capture_width=1920,capture_height=1080,display_width=1280,display_height=720,framerate=30,flip_method=0):
     return ("nvarguscamerasrc ! "
         "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "width={}, height={}, "
+        "format=NV12, framerate={}/1 ! "
+        "nvvidconv flip-method={} ! "
+        "video/x-raw, width={}, height={}, format=BGRx ! "
         "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink" % (
+        "video/x-raw, format=BGR ! appsink".format(
             capture_width,
             capture_height,
             framerate,
